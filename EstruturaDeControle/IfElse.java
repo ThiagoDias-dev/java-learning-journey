8b6e7583d901500a3ac1db78e7dc93781ccccaa6
Thiago Dias <118310221+ThiagoDias-dev@users.noreply.github.com>
2026-08-18 15:01:19 -0300

    Add IF Else

--- EstruturaDeControle/IfElse.java
+++ EstruturaDeControle/IfElse.java
@@ -1,12 +1,17 @@
 public class IfElse {
     public static void main(String[] args) {
+        
+        
         // IF (se) - ELSE (senão)
         String aluno = "Pedro"; // String para comparação
+        
         int idade = 17;        // Inteiro usado para comparação
 
         if (idade >= 18) {     // SE idade for maior ou igual a 18...
             System.out.println("Maior de idade"); // Se SIM, imprimirá isso
+        
         } else {
             System.out.println("Menor de idade"); // Se for menor, imprimirá isso
+        
         }
     }
