ef3e2d360f582d505fd441fbe714d11d2f57ec6b
Thiago Dias <118310221+ThiagoDias-dev@users.noreply.github.com>
2026-08-20 16:01:10 -0300

    laço for adcionado à pasta

---
+++ EstruturaDeControle/03 - For.java
@@ -0,0 +1,11 @@
+public class For{
+    public static void main(String[] args){
+
+     for (int Contagem = 0; Contagem <=9; Contagem++){ //(inicialização; condição; incremento)
+               System.out.println(Contagem);  //  Resultado atual
+     }
+     for (int ContagemRegressiva = 10; ContagemRegressiva >=0; ContagemRegressiva--){ //(inicialização; condição; decremento)
+                System.out.println(ContagemRegressiva); //  Resultado atual
+     }
+    }
+   }
