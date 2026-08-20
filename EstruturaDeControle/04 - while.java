64a3c07a56b9f6fe0ad6775fdf19102e2f58b683
Thiago Dias <118310221+ThiagoDias-dev@users.noreply.github.com>
2026-08-20 17:20:30 -0300

    adcionada a pasta while à pasta
    Implement while loop for password attempts until correct.

--- EstruturaDeControle/04 - while.java
+++ EstruturaDeControle/04 - while.java
@@ -1,1 +1,15 @@
-
+public class For{
+    public static void main(String[] args){
+        // Senha de inicialização
+     int senha = 0;
+        // Executa enquanto a condição for verdadeira (senha diferente de 5)
+     while(senha!=5) {
+        // Mostra a tentativa atual
+        System.out.println("Tentativa: " +senha);
+        // Incrementa mais 1 a cada tentativa
+        senha++;
+        }
+        // Execução quando a condição se torna falsa
+        System.out.println("Tentativa: 05. SENHA CORRETA! ");
+       }
+      }
