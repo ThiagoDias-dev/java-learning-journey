e90a3153eaef420f3739fdfa6137f46ef29b7eb4
Thiago Dias <118310221+ThiagoDias-dev@users.noreply.github.com>
2026-08-20 17:25:50 -0300

    Reescrevi a classe for

--- EstruturaDeControle/03 - For.java
+++ EstruturaDeControle/03 - For.java
@@ -1,15 +1,20 @@
 public class For{
+
     public static void main(String[] args){
-        // Senha de inicialização
-     int senha = 0;
-        // Executa enquanto a condição for verdadeira (senha diferente de 5)
-     while(senha!=5) {
-        // Mostra a tentativa atual
-        System.out.println("Tentativa: " +senha);
-        // Incrementa mais 1 a cada tentativa
-        senha++;
-        }
-        // Execução quando a condição se torna falsa
-        System.out.println("Tentativa: 05. SENHA CORRETA! ");
-       }
-      }
+
+     for (int Contagem = 0; Contagem <=9; Contagem++){ //(inicialização; condição; incremento)
+
+               System.out.println(Contagem); // Chamada do resultado da contagem
+
+     }
+
+     for (int ContagemRegressiva = 10; ContagemRegressiva >=0; ContagemRegressiva--){ //(inicialização; condição; incremento)
+
+                System.out.println(ContagemRegressiva); // Chamada do resultado da contagem
+
+     }
+
+    }
+
+   }
+
