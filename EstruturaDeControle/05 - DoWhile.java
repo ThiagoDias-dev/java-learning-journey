d3e28c7170414074cc4029dfd5817ad583ddccc9
Thiago Dias <118310221+ThiagoDias-dev@users.noreply.github.com>
2026-08-20 17:23:28 -0300

    Implementei DoWhile à minha pasta

--- EstruturaDeControle/05 - DoWhile.java
+++ EstruturaDeControle/05 - DoWhile.java
@@ -1,1 +1,19 @@
+public class DoWhile {
+    public static void main(String[] args) {
 
+        // Senha de inicialização
+        int senha = 0;
+
+        do {
+            // Mostra a tentativa atual
+            System.out.println("Tentativa: " + senha);
+
+            // Incrementa mais 1 a cada tentativa
+            senha++;
+
+        } while (senha != 5);
+
+        // Execução quando a condição se torna falsa
+        System.out.println("Tentativa: 5. SENHA CORRETA!");
+    }
+}
