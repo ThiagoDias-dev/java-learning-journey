2f07444d350d3f899405bbf0675ded7a7a2bee93
Thiago Dias <118310221+ThiagoDias-dev@users.noreply.github.com>
2026-08-20 16:58:35 -0300

    While adcionado à pasta

--- EstruturaDeControle/03 - For.java
+++ EstruturaDeControle/03 - For.java
@@ -1,11 +1,15 @@
 public class For{
     public static void main(String[] args){
-
-     for (int Contagem = 0; Contagem <=9; Contagem++){ //(inicialização; condição; incremento)
-               System.out.println(Contagem);  //  Resultado atual
-     }
-     for (int ContagemRegressiva = 10; ContagemRegressiva >=0; ContagemRegressiva--){ //(inicialização; condição; decremento)
-                System.out.println(ContagemRegressiva); //  Resultado atual
-     }
-    }
-   }
+        // Senha de inicialização
+     int senha = 0;
+        // Executa enquanto a condição for verdadeira (senha diferente de 5)
+     while(senha!=5) {
+        // Mostra a tentativa atual
+        System.out.println("Tentativa: " +senha);
+        // Incrementa mais 1 a cada tentativa
+        senha++;
+        }
+        // Execução quando a condição se torna falsa
+        System.out.println("Tentativa: 05. SENHA CORRETA! ");
+       }
+      }
